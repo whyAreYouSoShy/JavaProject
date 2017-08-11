b8d74d6a74ba5d42fbea30f3a2d6430934756b59
ygz <515605219@qq.com>
2017-08-11 17:27:58 +0800

    添加商品

--- taotao-parent/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-parent/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -1,13 +1,12 @@
 package com.taotao.controller;
 
 import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -41,6 +40,15 @@ public class ItemController {
         System.out.println("itemlist");
         return itemService.getItemList(page, rows);
     }
+
+    @RequestMapping(value="/item/save",method = RequestMethod.POST)
+    @ResponseBody
+    public TaotaoResult addItem(TbItem item,String desc) {
+        TaotaoResult taotaoResult = itemService.addItem(item, desc);
+        return taotaoResult;
+
+    }
+
 
 
 }
